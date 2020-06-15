f4fd68a88da23e16d32b479ca1d8d031d273987c
Ian Kachan <kachan.yanf@gmail.com>
2020-06-15 23:38:16 +0500

    Added the open, save as, save and close buttons and functionality for opening and reading text files

--- src/application/SimpleTextEditor.java
+++ src/application/SimpleTextEditor.java
@@ -1,16 +1,28 @@
 package application;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 public class SimpleTextEditor {
 
 	private JFrame frame;
-
+	private JTextArea textArea;
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -18,6 +30,8 @@ public class SimpleTextEditor {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					
 					SimpleTextEditor window = new SimpleTextEditor();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -44,13 +58,106 @@ public class SimpleTextEditor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JTextArea textArea = new JTextArea();
+		textArea = new JTextArea();
+		textArea.setFont(new Font("Arial", Font.PLAIN, 13));
 		frame.getContentPane().add(textArea, BorderLayout.NORTH);
 		
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		
+		JMenuBar menuBar = new JMenuBar();
+		frame.getContentPane().add(menuBar, BorderLayout.NORTH);
+		
+		JMenu mnFile = new JMenu("\u0424\u0430\u0439\u043B");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("\u041E\u0442\u043A\u0440\u044B\u0442\u044C");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				open();
+			}
+		});
+		mnFile.add(mntmNewMenuItem);
+		
+		JMenuItem menuItem = new JMenuItem("\u0421\u043E\u0445\u0440\u0430\u043D\u0438\u0442\u044C \u043A\u0430\u043A");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				create();
+			}
+		});
+		mnFile.add(menuItem);
+		
+		JMenuItem menuItem_1 = new JMenuItem("\u0421\u043E\u0445\u0440\u0430\u043D\u0438\u0442\u044C");
+		menuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
+		mnFile.add(menuItem_1);
+		
+		JMenuItem menuItem_2 = new JMenuItem("\u0417\u0430\u043A\u0440\u044B\u0442\u044C");
+		menuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				close();
+			}
+		});
+		mnFile.add(menuItem_2);
+		
 		
 	}
+	
+	
+	private void open() {
+		try {
+			
+			JFileChooser chooser = new JFileChooser();
+			chooser.setDialogTitle("Выбрать текстовый документ");
+			chooser.showOpenDialog(null);
+			
+			File selectedFile = chooser.getSelectedFile();
+			if (!selectedFile.exists()) {
+				//Добавить сообщение об ошибке
+				return;
+			}
+			
+			Scanner scanner = new Scanner(selectedFile);
+			String contents = "";
+			while (scanner.hasNextLine()) {
+				contents += scanner.nextLine()+"\n";
+			}
+			scanner.close();
+			textArea.setText(contents);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void create() {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void save() {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void close() {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 
 }
